0e78fc5dc4d61bb15f61dda2be347236d4c5b26d
Paras Yadav <62833829+parascoding@users.noreply.github.com>
2022-06-21 19:15:49 +0530

    Add files via upload

---
+++ quotes.js
@@ -0,0 +1,6 @@
+var quotes = [
+    "Be yourself; everyone else is already taken.",
+    "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
+    "You know you're in love when you can't fall asleep because reality is finally better than your dreams.",
+    "Be the change that you wish to see in the world."
+]
